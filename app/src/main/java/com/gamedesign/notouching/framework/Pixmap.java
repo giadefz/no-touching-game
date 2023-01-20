43fd9ca78f4397006cb9d2d38833944f11e32078
jeanjik <45577016+giadefz@users.noreply.github.com>
2023-01-20 15:51:10 +0100

    added framework from mrNom

---
+++ app/src/main/java/com/gamedesign/notouching/framework/Pixmap.java
@@ -0,0 +1,13 @@
+package com.gamedesign.notouching.framework;
+
+import com.gamedesign.notouching.framework.Graphics.PixmapFormat;
+
+public interface Pixmap {
+    public int getWidth();
+
+    public int getHeight();
+
+    public PixmapFormat getFormat();
+
+    public void dispose();
+}
