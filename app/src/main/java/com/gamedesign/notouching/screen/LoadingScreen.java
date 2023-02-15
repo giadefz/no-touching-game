a49f0f770d6d09b2b5c9b0841219a7ae8acffe19
stsivo-apple <ssivo22@fed.idserve.net>
2023-02-15 20:40:06 +0100

    Added various assets

--- app/src/main/java/com/gamedesign/notouching/screen/LoadingScreen.java
+++ app/src/main/java/com/gamedesign/notouching/screen/LoadingScreen.java
@@ -3,7 +3,6 @@ package com.gamedesign.notouching.screen;
 
 import com.gamedesign.notouching.framework.Game;
 import com.gamedesign.notouching.framework.Graphics;
-import com.gamedesign.notouching.framework.Music;
 import com.gamedesign.notouching.framework.Screen;
 import com.gamedesign.notouching.parse.GameObjectsJSON;
 import com.gamedesign.notouching.util.Assets;
@@ -29,13 +28,13 @@ public class LoadingScreen extends Screen {
         Assets.background = graphics.newPixmap("background.png", Graphics.PixmapFormat.RGB565);
         Assets.mainMenu = graphics.newPixmap("mainmenu.png", Graphics.PixmapFormat.ARGB4444);
         Assets.buttons = graphics.newPixmap("buttons.png", Graphics.PixmapFormat.ARGB4444);
-        Assets.tile = graphics.newPixmap("BridgeTiles.png", Graphics.PixmapFormat.ARGB4444);
+        Assets.tile = graphics.newPixmap("bridgeTile1.png", Graphics.PixmapFormat.ARGB4444);
         Assets.bomb = graphics.newPixmap("bomb.png", Graphics.PixmapFormat.ARGB4444);
         Assets.stopButton = graphics.newPixmap("stop.png", Graphics.PixmapFormat.ARGB4444);
         Assets.pauseButton = graphics.newPixmap("pause.png", Graphics.PixmapFormat.ARGB4444);
         Assets.playButton = graphics.newPixmap("play.png", Graphics.PixmapFormat.ARGB4444);
         Assets.retryButton = graphics.newPixmap("retry.png", Graphics.PixmapFormat.ARGB4444);
-        Assets.lvl1background = graphics.newPixmap("lvl1background.png", Graphics.PixmapFormat.RGB565);
+        Assets.lvl1background = graphics.newPixmap("levelBackground1.png", Graphics.PixmapFormat.RGB565);
         Assets.pier = graphics.newPixmap("pier.png", Graphics.PixmapFormat.RGB565);
         Assets.pier2 = graphics.newPixmap("pier2.png", Graphics.PixmapFormat.RGB565);
         Assets.wheel = graphics.newPixmap("wheel.png", Graphics.PixmapFormat.RGB565);
